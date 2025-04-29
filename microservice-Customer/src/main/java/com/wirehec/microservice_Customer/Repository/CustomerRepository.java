c1c2bf884b72863ffa6e223d4ba9c48a02e9eb02
Hector Olivera <hector.olivera@campusfp.es>
2025-04-29 17:32:32 +0200

    CRUD Configuration for Customer

--- microservice-Customer/src/main/java/com/wirehec/microservice_Customer/Repository/CustomerRepository.java
+++ microservice-Customer/src/main/java/com/wirehec/microservice_Customer/Repository/CustomerRepository.java
@@ -4,6 +4,14 @@ import com.wirehec.microservice_Customer.Entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+
+    @Override
+    List<CustomerEntity> findAll();
+
+    @Override
+    <S extends CustomerEntity> S save(S entity);
 }
--- microservice-Customer/src/main/java/com/wirehec/microservice_Customer/Service/Inter/ICustomerService.java
+++ microservice-Customer/src/main/java/com/wirehec/microservice_Customer/Service/Inter/ICustomerService.java
@@ -1,4 +1,14 @@
 package com.wirehec.microservice_Customer.Service.Inter;
 
+import com.wirehec.microservice_Customer.Entity.CustomerEntity;
+
+import java.util.List;
+
 public interface ICustomerService {
+
+    List<CustomerEntity> findAll();
+
+    CustomerEntity saveCustomer(CustomerEntity CustomerEntity);
+
+    void deleteCustomer(Long id);
 }
