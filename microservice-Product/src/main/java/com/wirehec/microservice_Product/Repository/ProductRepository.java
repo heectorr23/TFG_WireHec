bce98ae61117b3a6ea8bbd21ed33d7712d8f040b
Hector Olivera <hector.olivera@campusfp.es>
2025-05-06 20:16:47 +0200

    Initial Push

--- microservice-Product/src/main/java/com/wirehec/microservice_Product/Repository/ProductRepository.java
+++ microservice-Product/src/main/java/com/wirehec/microservice_Product/Repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.wirehec.microservice_Product.Repository;
 
+import com.wirehec.microservice_Product.Entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductRepository, Long> {
 }
--- microservice-Product/src/main/java/com/wirehec/microservice_Product/Service/Inter/IProductService.java
+++ microservice-Product/src/main/java/com/wirehec/microservice_Product/Service/Inter/IProductService.java
@@ -1,4 +1,10 @@
 package com.wirehec.microservice_Product.Service.Inter;
 
+import com.wirehec.microservice_Product.Entity.ProductEntity;
+
+import java.util.List;
+
 public interface IProductService {
+    List<ProductEntity> findByIdProveedor(Long idProveedor);
+
 }
