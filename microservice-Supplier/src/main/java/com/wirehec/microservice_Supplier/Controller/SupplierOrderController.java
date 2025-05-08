281de88c3767fd519474dbb1123f0ec332155c98
Hector Olivera <hector.olivera@campusfp.es>
2025-05-08 21:33:00 +0200

    Fix orderSupplier

--- microservice-Supplier/src/main/java/com/wirehec/microservice_Supplier/Controller/SupplierOrderController.java
+++ microservice-Supplier/src/main/java/com/wirehec/microservice_Supplier/Controller/SupplierOrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/Ordersupplier")
+@RequestMapping("/api/orderSupplier")
 @Slf4j
 public class SupplierOrderController {
 
